bec46b7e3bded1841d66b883e961f9ee084a7790
GaneshBarma <barmaganesh@gmail.com>
2022-04-17 17:51:50 +0530

    Logger

--- src/main/java/SciCal/My_Main.java
+++ src/main/java/SciCal/My_Main.java
@@ -5,7 +5,7 @@ import java.lang.Math;
 import java.util.*;
 public class My_Main 
 {
- //private static final Logger logger = LogManager.getLogger(My_Main.class);
+ private static final Logger logger = LogManager.getLogger(My_Main.class);
  public My_Main() {}
  public static void main(String[] args) 
  {
@@ -51,34 +51,34 @@ public class My_Main
 //These functions are not defined over the entire real range. So, for inputs out of the domains, nan is returned.
  public double root(double number1) 
  {
-  //logger.info("[ROOT] - " + number1); 
+  logger.info("[ROOT] - " + number1);
   double return_val = Math.sqrt(number1);
-  //logger.info("[RESULT - ROOT] - " + return_val);
+  logger.info("[RESULT - ROOT] - " + return_val);
   return return_val;
  }
  public double factorial(int number1) 
  {
-  //logger.info("[FACTORIAL] - " + number1);  
+  logger.info("[FACTORIAL] - " + number1);
   if(number1<0)
    return Double.NaN;
   double fact=1;  
   for(int i=1;i<=number1;i++)    
    fact=fact*i;   
-  //logger.info("[RESULT - FACTORIAL] - " + fact);  
+  logger.info("[RESULT - FACTORIAL] - " + fact);
   return fact;
  }
  public double log(double number1) 
  {
-  //logger.info("[LOG] - " + number1);    
+  logger.info("[LOG] - " + number1);
   double return_val = Math.log(number1);
-  //logger.info("[RESULT - LOG] - " + return_val); 
+  logger.info("[RESULT - LOG] - " + return_val);
   return return_val;
  }
  public double power(double number1, double number2) 
  {
-  //logger.info("[POWER] - " + number1 + ", " + number2);     
+  logger.info("[POWER] - " + number1 + ", " + number2);
   double return_val = Math.pow(number1 , number2);
-  //logger.info("[RESULT - POWER] - " + return_val);  
+  logger.info("[RESULT - POWER] - " + return_val);
   return return_val;
  }
 }
